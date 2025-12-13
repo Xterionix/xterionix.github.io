616ae265ffe4de3e3afa834038a03686973fdd09
Xterionix <xterionyt@gmail.com>
2025-12-12 23:37:15 -0600

    - Moved all data on videos out rather than hardcoding it into the html

---
+++ scripts/data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    id: 'main-gallery',
+    entries: [
+      { id: "crafting-table", title: "4x4 Crafting Table", type: "video", src: "4x Crafting Table.mp4", poster: "4x Crafting Table.png", tags: "scripts,blocks", credit: true },
+      { title: "Portal", type: "video", src: "Portal.mp4", poster: "Portal.png", tags: "scripts", credit: false },
+      { title: "Analyzer", type: "video", src: "Analyzer.mp4", poster: "Analyzer.png", tags: "scripts,blocks", credit: true },
+      { title: "Drone", type: "video", src: "Drone.mp4", poster: "Drone.png", tags: "scripts,entities", credit: false },
+      { title: "Item Stealing", type: "video", src: "Item Stealing.mp4", poster: "Item Stealing.png", tags: "scripts,entities", credit: false },
+      { title: "DNA Sequencer", type: "video", src: "DNA Sequencer.mp4", poster: "DNA Sequencer.png", tags: "scripts,blocks", credit: true },
+      { title: "Cultivator", type: "video", src: "Cultivator.mp4", poster: "Cultivator.png", tags: "scripts,blocks", credit: false },
+      { title: "Build a Brad", type: "video", src: "Build a brad.mp4", poster: "Build a brad.png", tags: "scripts,blocks", credit: false },
+      { title: "Stealing Items from Chests", type: "video", src: "Chest Thief.mp4", poster: "Chest Thief.png", tags: "scripts,entities", credit: false },
+      { title: "Custom Shulker Box", type: "video", src: "Shulker.mp4", poster: "Shulker.png", tags: "scripts,blocks", credit: false },
+      { title: "Car Fuelling System", type: "video", src: "Jeep Fueling.mp4", poster: "Jeep Fueling.png", tags: "scripts,entities", credit: true },
+      { title: "Encyclopedia", type: "video", src: "Encyclopedia.mp4", poster: "Encyclopedia.png", tags: "scripts", credit: true },
+      { title: "On Tame Encylopedia", type: "video", src: "Tame dossiers.mp4", poster: "Tame dossiers.png", tags: "scripts", credit: true },
+      { title: "Explosive Pickaxe", type: "video", src: "Explosive Pickaxe.mp4", poster: "Explosive Pickaxe.png", tags: "scripts,items", credit: false },
+      { title: "Multiblock Pickaxe", type: "video", src: "Multiblock Pickaxe.mp4", poster: "Multiblock Pickaxe.png", tags: "scripts,items", credit: false },
+      { title: "Cosmetics Menu", type: "video", src: "Cosmetics.mp4", poster: "Cosmetics.png", tags: "scripts", credit: false },
+      { title: "Pokeball", type: "video", src: "Pokeballs.mp4", poster: "Pokeballs.png", tags: "scripts", credit: false },
+      { title: "Border", type: "video", src: "Border.mp4", poster: "Border.png", tags: "functions", credit: false },
+      { title: "Flipbook Block", type: "video", src: "Gears.mp4", poster: "Gears.png", tags: "blocks", credit: false },
+      { title: "Ore Respawning", type: "video", src: "Ore Respawn.mp4", poster: "Ore Respawn.png", tags: "functions", credit: false },
+      { title: "Rideable Flying Entity", type: "video", src: "Pterosaur Flight Controls.mp4", poster: "Pterosaur Flight Controls.png", tags: "entities", credit: false },
+      { title: "Mate Animations", type: "video", src: "Mate Animations.mp4", poster: "Mate Animations.png", tags: "entities", credit: false },
+      { title: "Look at Entity", type: "video", src: "Medusa.mp4", poster: "Medusa.png", tags: "scripts,entities", credit: false },
+      { title: "Vein Miner", type: "video", src: "Veinminer.mp4", poster: "Vein Miner.png", tags: "scripts", credit: false },
+      { title: "Tree Capitator", type: "video", src: "Tree Capitator.mp4", poster: "Tree Capitator.png", tags: "scripts", credit: false },
+      { title: "Bucketable Entity", type: "video", src: "Bucketable Mobs.mp4", poster: "Bucketable Mobs.png", tags: "entities", credit: false },
+      { title: "Nether MLG", type: "video", src: "Nether MLG.mp4", poster: "Nether MLG.png", tags: "scripts", credit: false },
+      { title: "Armor with Effects", type: "video", src: "Enderite.mp4", poster: "Enderite.png", tags: "items,functions", credit: false },
+      { title: "Ores", type: "video", src: "Ores.mp4", poster: "Ores.png", tags: "blocks,loot", credit: false },
+      { title: "Stalking Entity", type: "video", src: "Stalk.mp4", poster: "Stalk.png", tags: "entities", credit: false },
+      { title: "Saving Ally Behavior", type: "video", src: "Glass Break.mp4", poster: "Glass Break.png", tags: "entities", credit: false },
+      { title: "Underwater Mount", type: "video", src: "Underwater Mount.mp4", poster: "Underwater Mount.png", tags: "entities", credit: false },
+      { title: "Secondary Attack", type: "video", src: "Secondary Attack.mp4", poster: "Secondary Attack.png", tags: "entities", credit: false },
+      { title: "Dragon Ride Attack", type: "video", src: "Dragon Ride Attack.mp4", poster: "Dragon Ride Attack.png", tags: "entities", credit: false },
+      { title: "Flickering Lights", type: "video", src: "Flickering Lights.mp4", poster: "Flickering Lights.png", tags: "blocks", credit: false },
+      { title: "Player Slide", type: "video", src: "Slide.mp4", poster: "Slide.png", tags: "entities", credit: false },
+      { title: "Locked Chests", type: "video", src: "Locked chests.mp4", poster: "Locked chests.png", tags: "scripts", credit: false },
+      { title: "Health Based Textures", type: "video", src: "Scar.mp4", poster: "Scar.png", tags: "entities", credit: false },
+      { title: "Ride Attack", type: "video", src: "Rider Attack.mp4", poster: "Rider Attack.png", tags: "entities", credit: false },
+      { title: "Custom Doors", type: "video", src: "Doors.mp4", poster: "Doors.png", tags: "scripts,blocks", credit: false },
+      { title: "Entity Interaction", type: "video", src: "Entity Interaction.mp4", poster: "Entity Interaction.png", tags: "entities", credit: false },
+      { title: "Custom Vines", type: "video", src: "Growing vines.mp4", poster: "Growing vines.png", tags: "blocks", credit: false },
+      { title: "Custom Beehive-esque Block", type: "video", src: "Pixie Hive.mp4", poster: "Pixie Hive.png", tags: "entities,blocks", credit: false },
+      { title: "Temperature System", type: "video", src: "Temperature.mp4", poster: "Temperature.png", tags: "functions,ui", credit: false },
+      { title: "Custom Armor and Weapon with 3D Models", type: "image", src: "img/addons/Custom Armor and Weapon with 3D Models.png", tags: "items", credit: false }
+    ]
+  },
+  {
+    id: 'marketplace-gallery',
+    entries: [
+      { title: "", type: "image", src: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/92ddc25c-2842-4548-9b8f-8eecbbc7cca7/FantacyWeapons_Thumbnail_0.jpg", link: "https://www.minecraft.net/en-us/marketplace/pdp?id=64af0338-b643-4a99-ad36-ebfb263942b1", layout: "alternate", subtitle: "Bug fixes only" },
+      { title: "", type: "image", src: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/19c4bacb-d92f-4f97-ad9f-6b1a1edf74b4/CLAWS_Thumbnail_0.jpg", link: "https://www.minecraft.net/en-us/marketplace/pdp?id=f1a0c6b6-8499-429e-b4b6-0d5e1f16f3f3", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/fabb8633-2883-48df-8af8-4256c1cac928/WorldUtilities_Thumbnail_0.jpg", link: "https://www.minecraft.net/en-us/marketplace/pdp?id=cd927ddf-5334-4b9a-8be4-b2cd0c57004f", layout: "center", subtitle: null },
+      { title: "Paleocraft: Dinosaur Breakout", type: "image", src: "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/12f4137a-ffb8-4211-88ff-a2b9ea22a550/Breakout_Thumbnail_0.jpg", link: "https://www.minecraft.net/en-us/marketplace/pdp?id=026e14b6-f712-4f7b-80b9-4e2a57c50fd7", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://xforgeassets002.xboxlive.com/pf-namespace-b63a0803d3653643/001eeaeb-0a75-4a9b-8860-9d7faaf3c3ed/Alien_Thumbnail_0.jpg", link: "https://www.minecraft.net/en-us/marketplace/pdp/compycraft/alien-apocalypse-add--on/18525fe0-60b8-4f52-93f6-d83c933913a4", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/9187b54b-c059-4085-a07c-c5f1a7da6fa2/Dinosaur_Expansion_thumbnail_0.jpg", link: "https://www.minecraft.net/en-us/marketplace/pdp/cyclone/primal-overworld-1.0/0c5e69b9-feea-4906-adfe-2a4f1c20075d", layout: "center", subtitle: null }
+    ]
+  },
+  {
+    id: 'published-gallery',
+    entries: [
+      { title: "Prehistoric Animalia", type: "image", src: "https://api.mcpedl.com/storage/submissions/76952/105/pa2_1-520x245.png", link: "https://mcpedl.com/prehistoric-animalia-addon/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/87252/102/pv3_1-520x245.png", link: "https://mcpedl.com/prehistoric-vanilla/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://mcpedl.com/wp-content/uploads/2021/02/tamable-foxes_1.png", link: "https://mcpedl.com/tamable-foxes-addon/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://media.forgecdn.net/attachments/875/548/mcpedl.png", link: "https://mcpedl.com/true-enderite/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/33780/102/page_1-520x245.png", link: "https://mcpedl.com/pink-river-dolphins-addon/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/88025/100/dc_1-520x245.png", link: "https://mcpedl.com/domesticraft/", layout: "center", subtitle: null },
+      { title: "Dragonology", type: "image", src: "https://api.mcpedl.com/storage/submissions/80257/100/dragonstuff003_1-520x245.png", link: "https://mcpedl.com/dragonology-a-httyd-addon/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/91405/100/pokeballs_1-520x245.png", link: "https://mcpedl.com/pokeballs/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/76329/103/ml_1-520x245.png", link: "https://mcpedl.com/mystic-land-addon/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/102263/100/c7ee5053399144639b18774743da61c5_1-520x245.png", link: "https://mcpedl.com/flourishing-fauna/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/30414/104/52-sin-ttulo20221222114054_1-520x245.png", link: "https://mcpedl.com/mesozoic-biology-addon/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/100186/100/1673167314674_1-520x245.png", link: "https://mcpedl.com/vhagar/", layout: "center", subtitle: null },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/73089/112/20220527113053_1-520x245.png", link: "https://mcpedl.com/ravemons-mob-addon/", layout: "alternate", subtitle: "Contributor" },
+      { title: "", type: "image", src: "https://api.mcpedl.com/storage/submissions/103952/100/image_1-520x245.png", link: "https://mcpedl.com/the-lost-world/", layout: "alternate", subtitle: "Contributor" }
+    ]
+  }
+]
